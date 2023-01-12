0de20a85c472c16025913a3194dc5dde5d1e5a7b
Mathieu Graillot <mathieu.graillot@ynov.com>
2023-01-12 14:34:55 +0100

    Create file_test (pytest)

---
+++ farmer_test.py
@@ -0,0 +1,14 @@
+# FILE FOR FUNCTION == TEST
+# complete with functions and tests
+
+
+# =====================
+# Examples
+# =====================
+
+def test_Calculator1():
+    assert 2 + 1 == 5
+
+
+def test_Calculator2():
+    assert 2 + 1 == 3
