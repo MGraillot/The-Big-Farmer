2f6c55446d9142b617368977a423ff3a8f034ad2
anton.nab <anton.nabholz@ynov.com>
2023-01-20 12:39:50 +0100

    Test Arroser_localisation V1

--- test_farmer.py
+++ test_farmer.py
@@ -1,18 +1,28 @@
 # FILE FOR FUNCTION == TEST
 # complete with functions and tests
 
-# import class_Fonctions
+from class_Fonctions import Ferme
 
 # =====================
 # Examples
 # =====================
 
 
-def test_Calculator1():
-    assert 2 + 1 == 3
+def test_arroser_localisation():
+    ferme = Ferme("Ferme_1")
+    ouvrier = 1
+    champs = 1
+    ferme._commands.clear()
+    ferme.arroser_localisation(ouvrier, champs)
+    assert ferme._commands == ("1 ARROSER 1")
 
 
-def test_nom():
-    x = "To"
-    y = "Thomas"
-    assert x in y
+def test_arroser_localisation_bis():
+    ferme = Ferme("Ferme_1")
+    ouvrier = 3
+    champs = 2
+    ferme._commands.clear()
+
+    ferme.arroser_localisation(ouvrier, champs)
+
+    assert ferme._commands == ("3 ARROSER 2")
