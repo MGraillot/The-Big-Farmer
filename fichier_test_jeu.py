dc8c84574caf963a71fba175578b360bdd63af82
Bastien Lagrange <bastien.lagrange33@gmail.com>
2023-01-26 12:47:39 +0100

    Ajout add loggin

--- fichier_test_jeu.py
+++ fichier_test_jeu.py
@@ -1,3 +1,4 @@
+import logging
 import argparse
 from class_Fonctions import Ferme
 from typing import NoReturn
@@ -40,5 +41,11 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-
+    logging.basicConfig(
+        filename="THE BIG FARMER.log",
+        encoding="utf-8",
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)-8s] %(filename)20s(%(lineno)3s):%(funcName)-20s :: %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+    )
     client = PlayerGameClient(args.address, args.port).run()
